410427fa182e8501e4b6b80f468ae1af7c996460
louisj <louis@vps540079.ovh.net>
2018-07-09 23:26:26 +0200

    add poller

---
+++ add_poller.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: $0 [IP] [server name]"
+  exit 1
+fi
+ssh-copy-id root@$1 -p 22
+ssh -p 22 root@$1
+apt-get install python-simplejson
+exit
+echo "$2 ansible_host=$1 ansible_user=root >> /opt/hosts"
+cd /opt/HoneyPy/config && ansible -m ping slaves
