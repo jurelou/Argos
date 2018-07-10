aabe8077ae2eadaa1592b559f42632be969c9443
Louis <louis.jurczyk@epitech.eu>
2018-07-10 11:15:10 +0200

    Update add_poller.sh

--- add_poller.sh
+++ add_poller.sh
@@ -8,3 +8,6 @@ fi
 ssh-copy-id root@$1 -p 22
 ssh -p 22 root@$1 "apt-get install python-simplejson"
 echo "$2 ansible_host=$1 ansible_user=root" >> /opt/hosts
+cd /opt/HoneyPy/config
+ansible -m ping slaves
+ansible-playbook -K install.yml 
